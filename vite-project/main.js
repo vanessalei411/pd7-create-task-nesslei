daaf2315143a8bae27d97650221bd3b6dd134e03
nesslei <botchedbunnies@gmail.com>
2023-02-15 13:29:02 -0500

    awvd
    wavdva

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,15 +1,16 @@
 import "./style.css";
 
-
 function myFunction() {
-  points.sort(function(a, b){return 0.5 - Math.random()});
+  points.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
   document.getElementById("response").innerHTML = points[0];
-  document.getElementById("response").style.fontSize="18px";
+  document.getElementById("response").style.fontSize = "18px";
   setTimeout(timeup, 4000);
+}
 
-function timeup(){
-    document.getElementById("response").innerHTML="8";
-    document.getElementById("response").style.fontSize="120px";
-    document.getElementById("clear").value="";
-
+function timeup() {
+  document.getElementById("response").innerHTML = "8";
+  document.getElementById("response").style.fontSize = "120px";
+  document.getElementById("clear").value = "";
 }
