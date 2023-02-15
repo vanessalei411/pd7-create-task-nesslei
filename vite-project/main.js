861b54de2e5e85b7453d8621cfeb5a55a46219fb
nesslei <botchedbunnies@gmail.com>
2023-02-15 13:28:15 -0500

    4g3gg3
    34w

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,15 @@
 import "./style.css";
 
-i have to watch a yotube video for this 
+
+function myFunction() {
+  points.sort(function(a, b){return 0.5 - Math.random()});
+  document.getElementById("response").innerHTML = points[0];
+  document.getElementById("response").style.fontSize="18px";
+  setTimeout(timeup, 4000);
+
+function timeup(){
+    document.getElementById("response").innerHTML="8";
+    document.getElementById("response").style.fontSize="120px";
+    document.getElementById("clear").value="";
+
+}
